f654824b6e93d37b9d25b4944bfec605d361055b
bingoori <sdwoo90@gmail.com>
2021-11-03 16:04:25 +0900

    eslint, prettier 적용

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 react + firebase + express + Okex api 활용
 기능 : 회원가입, 로그인, 주문, 조회
+
+ISSUE
+
+1. 최신 eslint 버전과 구버전 node 호환 이슈
+  - node 업데이트
+  
+2. eslint 8.1.0 버전호환 이슈
+  - eslint 7.11.0로 고정
+  - react-scripts 4.0.0, 4.0.3로 고정
+  - 추가 이슈 발생시 @babel/core 7.12.3 설치
--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank" rel="noreferrer"
+				>
+          Learn Reactcc 
+				</a>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
